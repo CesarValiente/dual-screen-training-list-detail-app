ab1290030417ea1781450b95e4309c984ffed2e1
Cesar Valiente <cevalien@microsoft.com>
2020-10-05 15:57:15 +0200

    Small improvement to visualize the item that has been currenlty clicked and slected when in dual screen mode using our shared-content view model.

--- app/src/main/java/com/surfaceduo/training/listitems/SharedVM.kt
+++ app/src/main/java/com/surfaceduo/training/listitems/SharedVM.kt
@@ -6,8 +6,13 @@ import com.surfaceduo.training.listitems.recyclerview.Item
 
 class SharedVM : ViewModel() {
     val selectedItem = MutableLiveData<Item>()
+    val selectedItemPosition = MutableLiveData<Int>()
 
     fun setSelectedItem(item: Item) {
         selectedItem.value = item
+    }
+
+    fun setSelectedItemPosition(position: Int) {
+        selectedItemPosition.value = position
     }
 }
